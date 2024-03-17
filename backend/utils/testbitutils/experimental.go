e66f13792a2e1ca6ba51d05708817be9e20bea2c
Thom <thomvanoorschot@hotmail.com>
2024-03-17 16:00:37 +0100

    Added tests to the 5 minute segments

--- backend/utils/testbitutils/experimental.go
+++ backend/utils/testbitutils/experimental.go
@@ -1,7 +1,6 @@
 package testbitutils
 
 import (
-	"fmt"
 	"math"
 	"time"
 )
@@ -25,20 +24,22 @@ const (
 	SegmentsInOneTwelveDay = SegmentsInOneSixthDay / 2
 )
 
-func BitsToSlotStarts(days []SplitUpDay, allowInvalidSegments bool, minimumSegments int8) []time.Time {
-	var startTimes []time.Time
+func BitsToSlotStarts(days []SplitUpDay, allowInvalidSegments bool, minimumSegments int16) []time.Time {
+	startTimes := make([]time.Time, 0, 24)
 	mask := createBitmask(minimumSegments)
+	today := time.Now().Local()
 	for i, day := range days {
 		if i == len(days)-1 && day.PartSix == 0 {
 			continue
 		}
-		today := time.Now().Local().Add(time.Duration(i) * 24 * time.Hour)
+		today.Add(time.Duration(i) * 24 * time.Hour)
 		startOfDay := time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location())
 		day.PartOne |= day.PartTwo << SegmentsInOneSixthDay
 		//if i < (len(days) - 1) {
 		//	day.PartTwo |= days[i+1].PartOne << 48
 		//}
-		var cursor int
+		var cursor int16
+		var lostFlippedBits int16
 		for si := 1; si <= SegmentsInDay; si++ {
 			if si != 1 {
 				mask = mask << uint(1)
@@ -46,39 +47,51 @@ func BitsToSlotStarts(days []SplitUpDay, allowInvalidSegments bool, minimumSegme
 
 			if si%SegmentsInOneTwelveDay == 0 && si != SegmentsInOneTwelveDay {
 				mask >>= SegmentsInOneTwelveDay
+				lostFlippedBits += countFlippedBits(day.PartOne & 0b111111111111111111111111)
+
 				day.PartOne >>= SegmentsInOneTwelveDay
 				switch si {
 				case 48:
 					day.PartOne |= day.PartTwo << SegmentsInOneTwelveDay
 				case 72:
+					day.PartOne |= day.PartTwo
 					day.PartOne |= day.PartThree << SegmentsInOneSixthDay
 				case 96:
 					day.PartOne |= day.PartThree << SegmentsInOneTwelveDay
 				case 120:
+					day.PartOne |= day.PartThree
 					day.PartOne |= day.PartFour << SegmentsInOneSixthDay
 				case 144:
 					day.PartOne |= day.PartFour << SegmentsInOneTwelveDay
 				case 168:
+					day.PartOne |= day.PartFour
 					day.PartOne |= day.PartFive << SegmentsInOneSixthDay
 				case 192:
 					day.PartOne |= day.PartFive << SegmentsInOneTwelveDay
 				case 216:
+					day.PartOne |= day.PartFive
 					day.PartOne |= day.PartSix << SegmentsInOneSixthDay
 				case 240:
 					day.PartOne |= day.PartSix << SegmentsInOneTwelveDay
-
+				case 264:
+					day.PartOne |= day.PartSix
 				}
-				//day.PartOne |= day.PartTwo << SegmentsInOneTwelveDay
 				cursor -= SegmentsInOneTwelveDay
 			} else {
-				cursor++
 			}
-			fmt.Printf("availability      : %064b\n", day.PartOne)
-			fmt.Printf("mask              : %064b\n", mask)
-			consecutiveOnes := countFlippedBitsUntilIndex(day.PartOne, si-1)
-			fmt.Printf("consecutiveOnes   : %d\n", consecutiveOnes)
+			cursor++
+			//fmt.Printf("availability      : %064b\n", day.PartOne)
+			//fmt.Printf("mask              : %064b\n", mask)
 			satisfiesMask := (day.PartOne & mask) == mask
-			if !satisfiesMask || (!allowInvalidSegments && countFlippedBitsUntilIndex(day.PartOne, cursor-1)%minimumSegments != 0) {
+			if !satisfiesMask {
+				continue
+			}
+			flippedBits := countFlippedBitsUntilIndex(day.PartOne, cursor-1)
+			if flippedBits == 0 {
+				lostFlippedBits = 0
+			}
+			//fmt.Printf("consecutiveOnes   : %d\n", flippedBits+lostFlippedBits)
+			if !allowInvalidSegments && (flippedBits+lostFlippedBits)%minimumSegments != 0 {
 				continue
 			}
 
@@ -88,17 +101,29 @@ func BitsToSlotStarts(days []SplitUpDay, allowInvalidSegments bool, minimumSegme
 	return startTimes
 }
 
-func createBitmask(minimumSegments int8) uint64 {
+func createBitmask(minimumSegments int16) uint64 {
 	return math.MaxUint64 >> (64 - minimumSegments)
 }
 
-func countFlippedBitsUntilIndex(num uint64, index int) int8 {
+func countFlippedBits(num uint64) int16 {
+	var count int16 = 0
+	for i := int16(0); i < 24; i++ {
+		if (num>>i)&1 == 1 {
+			count++
+		} else {
+			count = 0
+		}
+	}
+	return count
+}
+
+func countFlippedBitsUntilIndex(num uint64, index int16) int16 {
 	if index == 0 {
 		return 0
 	}
-	var count int8 = 0
+	var count int16 = 0
 
-	for i := 0; i < index; i++ {
+	for i := int16(0); i < index; i++ {
 		if (num>>(index-i-1))&1 == 1 {
 			count++
 		} else {
--- backend/utils/testbitutils/experimental_test.go
+++ backend/utils/testbitutils/experimental_test.go
@@ -7,110 +7,148 @@ import (
 
 // 0b111111111111111111111111111111111111111111111111
 // 0b000000000000000000000000000000000000000000000000
+// Each is 4 hours
+//var a1 uint64 = 0b100000000000000000000000000000000000000000000000 // 00:00 - 04:00
+//var a2 uint64 = 0b000000000000000000000000000000000000000000000001 // 04:00 - 08:00
+//var a3 uint64 = 0b000000000000000000000000000000000000000000000000 // 08:00 - 12:00
+//var a4 uint64 = 0b000000000000000000000000000000000000000000000000 // 12:00 - 16:00
+//var a5 uint64 = 0b000000000000000000000000000000000000000000000000 // 16:00 - 20:00
+//var a6 uint64 = 0b000000000000000000000000000000000000000000000000 // 20:00 - 24:00
 
-func TestFiveMinutes(t *testing.T) {
-	type expectedResult struct {
-		length     int
-		startTimes []time.Time
+var today = time.Now().Local()
+
+type expectedResult struct {
+	length     int
+	startTimes []time.Time
+}
+type definition struct {
+	name                 string
+	availability         []SplitUpDay
+	allowInvalidSegments bool
+	minimumSegments      int16
+	expectedResult       expectedResult
+}
+
+var definitions = []definition{
+	{
+		name: "end of part spills over to first part",
+		availability: []SplitUpDay{
+			{
+				PartOne:   0b100000000000000000000000000000000000000000000000,
+				PartTwo:   0b000000000000000000000000000000000000000000000001,
+				PartThree: 0b000000000000000000000000000000000000000000000000,
+				PartFour:  0b000000000000000000000000000000000000000000000000,
+				PartFive:  0b000000000000000000000000000000000000000000000000,
+				PartSix:   0b000000000000000000000000000000000000000000000000,
+			}, {},
+		},
+		allowInvalidSegments: false,
+		minimumSegments:      2,
+		expectedResult: expectedResult{
+			length:     1,
+			startTimes: []time.Time{time.Date(today.Year(), today.Month(), today.Day(), 3, 55, 0, 0, today.Location())},
+		},
+	},
+	{
+		name: "large minimum segment of 4 hours should still work",
+		availability: []SplitUpDay{
+			{
+				PartOne:   0b111110000000000000000000000000000000000000000000,
+				PartTwo:   0b000001111111111111111111111111111111111111111111,
+				PartThree: 0b000000000000000000000000000000000000000000000000,
+				PartFour:  0b000000000000000000000000000000000000000000000000,
+				PartFive:  0b000000000000000000000000000000000000000000000000,
+				PartSix:   0b000000000000000000000000000000000000000000000000,
+			}, {},
+		},
+		allowInvalidSegments: false,
+		minimumSegments:      48,
+		expectedResult: expectedResult{
+			length:     1,
+			startTimes: []time.Time{time.Date(today.Year(), today.Month(), today.Day(), 3, 35, 0, 0, today.Location())},
+		},
+	},
+	{
+		name: "handle gaps in availability",
+		availability: []SplitUpDay{
+			{
+				PartOne:   0b000000000000000000000000000000000000000000000000,
+				PartTwo:   0b000000000000000000000000000000000000000000000000,
+				PartThree: 0b000000000000000000001111111111110000111111111111,
+				PartFour:  0b000000000000000000000000000000000000000000000000,
+				PartFive:  0b000000000000000000000000000000000000000000000000,
+				PartSix:   0b000000000000000000000000000000000000000000000000,
+			}, {},
+		},
+		allowInvalidSegments: false,
+		minimumSegments:      12,
+		expectedResult: expectedResult{
+			length: 2,
+			startTimes: []time.Time{
+				time.Date(today.Year(), today.Month(), today.Day(), 8, 0, 0, 0, today.Location()),
+				time.Date(today.Year(), today.Month(), today.Day(), 9, 20, 0, 0, today.Location()),
+			},
+		},
+	},
+	{
+		name: "allow invalid segments doesn't skip",
+		availability: []SplitUpDay{
+			{
+				PartOne:   0b000000000000000000000000000000000000000000000000,
+				PartTwo:   0b000000000000000000000000000000000000000000000000,
+				PartThree: 0b000000000000000000000011110000000000000000000000,
+				PartFour:  0b000000000000000000000000000000000000000000000000,
+				PartFive:  0b000000000000000000000000000000000000000000000000,
+				PartSix:   0b000000000000000000000000000000000000000000000000,
+			}, {},
+		},
+		allowInvalidSegments: true,
+		minimumSegments:      2,
+		expectedResult: expectedResult{
+			length: 3,
+		},
+	},
+	{
+		name: "invalid segment in previous bit range",
+		availability: []SplitUpDay{
+			{
+				PartOne:   0b000000000000000000000000000000000000000000000000,
+				PartTwo:   0b111111111000000000000000000000000000000000000000,
+				PartThree: 0b111111111111111111111111111111111111111111111111,
+				PartFour:  0b000000000000000000000000000000000000000000000000,
+				PartFive:  0b000000000000000000000000000000000000000000000000,
+				PartSix:   0b000000000000000000000000000000000000000000000000,
+			}, {},
+		},
+		allowInvalidSegments: false,
+		minimumSegments:      12,
+		expectedResult: expectedResult{
+			length: 4,
+			startTimes: []time.Time{
+				time.Date(today.Year(), today.Month(), today.Day(), 7, 15, 0, 0, today.Location()),
+				time.Date(today.Year(), today.Month(), today.Day(), 8, 15, 0, 0, today.Location()),
+				time.Date(today.Year(), today.Month(), today.Day(), 9, 15, 0, 0, today.Location()),
+				time.Date(today.Year(), today.Month(), today.Day(), 10, 15, 0, 0, today.Location()),
+			},
+		},
+	},
+}
+
+func BenchmarkBitsToSlotStarts(b *testing.B) {
+	for _, testCase := range definitions {
+		b.Run(testCase.name, func(b *testing.B) {
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				BitsToSlotStarts(testCase.availability, testCase.allowInvalidSegments, testCase.minimumSegments)
+			}
+		})
 	}
-	type definition struct {
-		name                 string
-		availability         []SplitUpDay
-		allowInvalidSegments bool
-		minimumSegments      int8
-		expectedResult       expectedResult
-	}
+}
 
-	// Each is 4 hours
-	//var a1 uint64 = 0b100000000000000000000000000000000000000000000000 // 00:00 - 04:00
-	//var a2 uint64 = 0b000000000000000000000000000000000000000000000001 // 04:00 - 08:00
-	//var a3 uint64 = 0b000000000000000000000000000000000000000000000000 // 08:00 - 12:00
-	//var a4 uint64 = 0b000000000000000000000000000000000000000000000000 // 12:00 - 16:00
-	//var a5 uint64 = 0b000000000000000000000000000000000000000000000000 // 16:00 - 20:00
-	//var a6 uint64 = 0b000000000000000000000000000000000000000000000000 // 20:00 - 24:00
-
-	today := time.Now().Local()
+func TestBitsToSlotStarts(t *testing.T) {
 	t.Parallel()
-	for _, testCase := range []definition{
-		{
-			name: "end of part spills over to first part",
-			availability: []SplitUpDay{
-				{
-					PartOne:   0b100000000000000000000000000000000000000000000000,
-					PartTwo:   0b000000000000000000000000000000000000000000000001,
-					PartThree: 0b000000000000000000000000000000000000000000000000,
-					PartFour:  0b000000000000000000000000000000000000000000000000,
-					PartFive:  0b000000000000000000000000000000000000000000000000,
-					PartSix:   0b000000000000000000000000000000000000000000000000,
-				}, {},
-			},
-			allowInvalidSegments: false,
-			minimumSegments:      2,
-			expectedResult: expectedResult{
-				length:     1,
-				startTimes: []time.Time{time.Date(today.Year(), today.Month(), today.Day(), 3, 55, 0, 0, today.Location())},
-			},
-		},
-		{
-			name: "large minimum segment of 4 hours should still work",
-			availability: []SplitUpDay{
-				{
-					PartOne:   0b111110000000000000000000000000000000000000000000,
-					PartTwo:   0b000001111111111111111111111111111111111111111111,
-					PartThree: 0b000000000000000000000000000000000000000000000000,
-					PartFour:  0b000000000000000000000000000000000000000000000000,
-					PartFive:  0b000000000000000000000000000000000000000000000000,
-					PartSix:   0b000000000000000000000000000000000000000000000000,
-				}, {},
-			},
-			allowInvalidSegments: false,
-			minimumSegments:      48,
-			expectedResult: expectedResult{
-				length:     1,
-				startTimes: []time.Time{time.Date(today.Year(), today.Month(), today.Day(), 3, 35, 0, 0, today.Location())},
-			},
-		},
-		{
-			name: "handle gaps in availability",
-			availability: []SplitUpDay{
-				{
-					PartOne:   0b000000000000000000000000000000000000000000000000,
-					PartTwo:   0b000000000000000000000000000000000000000000000000,
-					PartThree: 0b000000000000000000001111111111110000111111111111,
-					PartFour:  0b000000000000000000000000000000000000000000000000,
-					PartFive:  0b000000000000000000000000000000000000000000000000,
-					PartSix:   0b000000000000000000000000000000000000000000000000,
-				}, {},
-			},
-			allowInvalidSegments: false,
-			minimumSegments:      12,
-			expectedResult: expectedResult{
-				length: 2,
-				startTimes: []time.Time{
-					time.Date(today.Year(), today.Month(), today.Day(), 8, 0, 0, 0, today.Location()),
-					time.Date(today.Year(), today.Month(), today.Day(), 9, 20, 0, 0, today.Location()),
-				},
-			},
-		},
-		{
-			name: "allow invalid segments doesn't skip",
-			availability: []SplitUpDay{
-				{
-					PartOne:   0b000000000000000000000000000000000000000000000000,
-					PartTwo:   0b000000000000000000000000000000000000000000000000,
-					PartThree: 0b000000000000000000001111111111111111111111111111,
-					PartFour:  0b000000000000000000000000000000000000000000000000,
-					PartFive:  0b000000000000000000000000000000000000000000000000,
-					PartSix:   0b000000000000000000000000000000000000000000000000,
-				}, {},
-			},
-			allowInvalidSegments: true,
-			minimumSegments:      12,
-			expectedResult: expectedResult{
-				length: 17,
-			},
-		},
-	} {
+
+	for _, testCase := range definitions {
 		t.Run(testCase.name, func(t *testing.T) {
 			startTimes := BitsToSlotStarts(testCase.availability, testCase.allowInvalidSegments, testCase.minimumSegments)
 			if len(startTimes) != testCase.expectedResult.length {
